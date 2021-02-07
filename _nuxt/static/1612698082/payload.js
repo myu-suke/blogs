d6ce55d4ea75ddda704dde7ecc7d2341b11d1eab
myu-suke <myu-suke@users.noreply.github.com>
2021-02-07 11:41:23 +0000

    deploy: d70256f96e62a79cd0c46276dcb5932e65f029cb

---
+++ _nuxt/static/1612698082/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", {data:[{tags:["Vue.js","content","javascript","nuxt","pwa","runtimeCaching","tailwindcss","toLocaleString","vue-admin-template","vue-cli","プラグイン"]}],fetch:[],mutations:void 0});
